2387887d36d8469b7b2867cea83597e94f05714f
Abhinash PC <avinash16595@gmail.com>
2019-09-11 12:44:48 +0530

    final

---
+++ vue-ui-design/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <zippy>
+   <p slot="zippy-heading">This is the heading</p>
+     <p slot="zippy-content">This is the demo only You can use this component in any component, just use the zippy tag and you are blahhhhhhhhhhhh............</p>
+    </zippy>
+  </div>
+</template>
+
+<script>
+import zippy from './components/zippy';
+export default {
+  name: 'app',
+  components: {
+    zippy
+  },
+ 
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ vue-ui-design/src/components/zippy.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="zippy-container">
+    <button class="accordion"  v-on:click="display= !display">
+        <slot name="zippy-heading"></slot>
+    </button>
+    <div class="panel" v-show="display">
+        <slot name="zippy-content"></slot>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'zippy',
+  data(){
+      return{
+          display: false
+      }
+  },
+    methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.accordion {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
+}
+
+.active, .accordion:hover {
+  background-color: #ccc; 
+}
+
+.panel {
+  padding: 0 18px;
+  background-color: whitesmoke;
+  overflow: hidden;
+}
+</style>
